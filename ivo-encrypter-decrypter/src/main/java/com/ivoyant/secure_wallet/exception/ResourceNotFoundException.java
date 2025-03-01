7efad9415343b5426b59cf11aaeb94116862c0ad
Shashank GK <89540267+Shashank-GK@users.noreply.github.com>
2025-03-01 15:16:26 +0530

    Delete ivo-encrypter-decrypter directory

--- ivo-encrypter-decrypter/src/main/java/com/ivoyant/secure_wallet/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.ivoyant.secure_wallet.exception;
-
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String str) {
-        super(str);
-    }
-}
